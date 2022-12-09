7f48badd5d0f30ec981d4776c8680f51451f40a1
yuchao <yuchao@sunwinon.com>
2022-12-09 16:56:40 +0800

    add 2

---
+++ root.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World! \n");
+
+  printf("Hello World! \n");
+  printf("Hello World! \n");
+
+  return 0;
+}
